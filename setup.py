105b22de77d909dcfb7c78c06d017ce9544a863d
Electro707 <general@electro707.com>
2024-12-07 21:21:41 -0500

    Added new setup.py that should build the C library

--- setup.py
+++ setup.py
@@ -1,3 +1,47 @@
-# This file is only for backwards compatibility with pip
-from setuptools import setup
-setup()
+# This file is partially copied from https://stackoverflow.com/questions/42585210/extending-setuptools-extension-to-use-cmake-in-setup-py
+import os
+import pathlib
+from setuptools import Extension, setup
+from setuptools.command.build_ext import build_ext as build_ext_orig
+
+class CMakeExtension(Extension):
+    def __init__(self, name):
+        # don't invoke the original build_ext for this special extension
+        super().__init__(name, sources=[])
+
+
+class build_ext(build_ext_orig):
+    def run(self):
+        for ext in self.extensions:
+            self.build_cmake(ext)
+        super().run()
+
+    def build_cmake(self, ext):
+        cwd = pathlib.Path().absolute()
+
+        # these dirs will be created in build_py, so if you don't have
+        # any python sources to bundle, the dirs will be missing
+        build_temp = pathlib.Path(self.build_temp)
+        build_temp.mkdir(parents=True, exist_ok=True)
+        extdir = pathlib.Path(self.get_ext_fullpath(ext.name))
+        extdir.mkdir(parents=True, exist_ok=True)
+
+        # example of cmake args
+        cmake_args = [
+            # '-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + str(extdir.parent.absolute()),
+            '-DBUILD_PYPTOUCH=1'
+        ]
+
+        os.chdir(str(build_temp))
+        self.spawn(['cmake', str(cwd)] + cmake_args)
+        self.spawn(['cmake', '--build', '.'])
+        # Troubleshooting: if fail on line above then delete all possible
+        # temporary CMake files including "CMakeCache.txt" in top level dir.
+        os.chdir(str(cwd))
+
+
+setup(
+    ext_modules=[CMakeExtension('ptouch-print')],
+    cmdclass={
+        'build_ext': build_ext,
+    })
